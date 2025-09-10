c4817b75abb86f56547824cf4ba34dc313d7b1b8
Yehya Ahmed <yayahmed@duck.com>
2025-09-10 00:32:26 -0700

    simplify network setup

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,25 +1,17 @@
-{pkgs, ...}: let
+{ pkgs, ... }:
+let
   constants = import ../../constants.nix;
-in {
+in
+{
   networking = {
     hostName = "nixos-cm3588";
-    # Try static IP first, fall back to DHCP
-    useDHCP = false;
-    interfaces.${constants.network.interface} = {
-      useDHCP = true; # DHCP as fallback
-      ipv4.addresses = [
-        {
-          address = constants.network.staticIP;
-          prefixLength = 22;
-        }
-      ];
-    };
-    # Gateway and DNS with fallbacks
-    defaultGateway = constants.network.gateway;
+    useNetworkd = true;
+    interfaces.${constants.network.interface}.useDHCP = true;
+
     nameservers = [
-      "9.9.9.9"
-      "1.1.1.1"
       constants.network.staticIP
+      "9.9.9.9" # Quad9
+      "1.1.1.1" # Cloudflare
     ];
   };
 
