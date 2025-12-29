b4bc74725c71fe11d898f5ee239d5d3c5c63b906
Yehya Ahmed <yayahmed@duck.com>
2025-12-28 16:42:38 -0800

    Remove undeeded dependencies

--- modules/programs/nix-tools.nix
+++ modules/programs/nix-tools.nix
@@ -1,34 +1,15 @@
 { pkgs, ... }:
 {
-  # Install Nix development tools system-wide
   environment.systemPackages = with pkgs; [
     nixd
-
-    # Formatters & linters
-    nixfmt-rfc-style 
-    statix # 
-    deadnix 
-
-    # Nix utilities
-    nixpkgs-review
-    nix-tree
-    nix-init
-    nix-update
+    nixfmt-rfc-style
   ];
-
+  
   programs = {
-    nix-ld = {
-      enable = true;
-      libraries = with pkgs; [
-        stdenv.cc.cc.lib
-        nodejs_20
-      ];
-    };
-
     git.enable = true;
     zsh.enable = true;
   };
-
+  
   # Use nixfmt as the default formatter for Nix IDEs
   environment.variables.NIX_FORMATTER = "nixfmt";
 }
